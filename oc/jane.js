54e38c10849d305b16dcd195538220e2925da631
Wintermetal <lauri.a.sarkioja@gmail.com>
2021-06-14 20:47:41 +0300

    oc update

--- oc/jane.js
+++ oc/jane.js
@@ -4,18 +4,12 @@ let bellinfolist = document.getElementById("bellinfolist")
 
 let bellb = document.getElementById("bellb");
 let bellr = document.getElementById("bellr");
-let bellheaderb = document.getElementById("bellheader");
+let bellheader = document.getElementById("bellheader");
 
 bellr.style.display = "none";
 
-bellheader.addEventListener("mouseover", () => changeColor(bellheader, "#818181"));
-bellheader.addEventListener("mouseout", () => changeColor(bellheader, "#FFFFFF"));
 bellheader.addEventListener("click", () => toggleData(bellb));
 bellheader.addEventListener("click", () => toggleData(bellr));
-
-function changeColor(elem, color){
-    elem.style.color = color;
-};
 
 function toggleData(item) {
     if(item.style.display === "none"){
