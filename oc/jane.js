255c8171faa400b1b17305c3553b794b8bfadb49
Wintermetal <lauri.a.sarkioja@gmail.com>
2021-06-15 04:57:31 +0300

    oc update

--- oc/jane.js
+++ oc/jane.js
@@ -2,12 +2,17 @@ const yoink = console.log;
 
 let bellinfolist = document.getElementById("bellinfolist")
 
+let bellhb = document.getElementById("bellhb");
+let bellhr = document.getElementById("bellhr");
 let bellb = document.getElementById("bellb");
 let bellr = document.getElementById("bellr");
 let bellheader = document.getElementById("bellheader");
 
+bellhr.style.display = "none";
 bellr.style.display = "none";
 
+bellheader.addEventListener("click", () => toggleData(bellhb));
+bellheader.addEventListener("click", () => toggleData(bellhr));
 bellheader.addEventListener("click", () => toggleData(bellb));
 bellheader.addEventListener("click", () => toggleData(bellr));
 
