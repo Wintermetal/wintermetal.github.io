1e88bc7bdcf4cd48481ca3104e5aacfc824a27e2
Wintermetal <lauri.a.sarkioja@gmail.com>
2021-11-20 14:41:18 +0200

    joulu

--- joulu/joulu.js
+++ joulu/joulu.js
@@ -7,11 +7,14 @@ let no3 = document.getElementById("no3");
 let link3 = document.getElementById("link3");
 let no4 = document.getElementById("no4");
 let link4 = document.getElementById("link4");
+let no5 = document.getElementById("no5");
+let link5 = document.getElementById("link5");
 
 link1.style.display = "none";
 link2.style.display = "none";
 link3.style.display = "none";
 link4.style.display = "none";
+link5.style.display = "none";
 
 no1.addEventListener("mouseover", () => changeColor(no1, "#E8282C"));
 no1.addEventListener("mouseout", () => changeColor(no1, "#000000"));
@@ -28,6 +31,10 @@ no3.addEventListener("click", () => toggleData(link3));
 no4.addEventListener("mouseover", () => changeColor(no4, "#6B8E23"));
 no4.addEventListener("mouseout", () => changeColor(no4, "#000000"));
 no4.addEventListener("click", () => toggleData(link4));
+
+no5.addEventListener("mouseover", () => changeColor(no5, "#E8282C"));
+no5.addEventListener("mouseout", () => changeColor(no5, "#000000"));
+no5.addEventListener("click", () => toggleData(link5));
 
 function changeColor(elem, color){
     elem.style.color = color;
