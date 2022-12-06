1239fb13d289f3b1cb4cbde0a4a813eb77dcfc17
Wintermetal <lauri.a.sarkioja@gmail.com>
2022-12-06 13:40:20 +0200

    joulu 22

--- joulu/joulu.js
+++ joulu/joulu.js
@@ -3,8 +3,8 @@ let no1 = document.getElementById("no1");
 let link1 = document.getElementById("link1");
 let no2 = document.getElementById("no2");
 let link2 = document.getElementById("link2");
-let no3 = document.getElementById("no3");
-let link3 = document.getElementById("link3");
+/*let no3 = document.getElementById("no3");
+let link3 = document.getElementById("link3");*/
 let no4 = document.getElementById("no4");
 let link4 = document.getElementById("link4");
 let no5 = document.getElementById("no5");
@@ -12,7 +12,7 @@ let link5 = document.getElementById("link5");
 
 link1.style.display = "none";
 link2.style.display = "none";
-link3.style.display = "none";
+/*link3.style.display = "none";*/
 link4.style.display = "none";
 link5.style.display = "none";
 
@@ -23,11 +23,11 @@ no1.addEventListener("click", () => toggleData(link1));
 no2.addEventListener("mouseover", () => changeColor(no2, "#E8282C"));
 no2.addEventListener("mouseout", () => changeColor(no2, "#000000"));
 no2.addEventListener("click", () => toggleData(link2));
-
+/*
 no3.addEventListener("mouseover", () => changeColor(no3, "#E8282C"));
 no3.addEventListener("mouseout", () => changeColor(no3, "#000000"));
 no3.addEventListener("click", () => toggleData(link3));
-
+*/
 no4.addEventListener("mouseover", () => changeColor(no4, "#6B8E23"));
 no4.addEventListener("mouseout", () => changeColor(no4, "#000000"));
 no4.addEventListener("click", () => toggleData(link4));
